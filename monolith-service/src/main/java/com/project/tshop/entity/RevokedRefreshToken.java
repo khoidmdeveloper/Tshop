43d42dba772314e46f9567e97f9af1aa2e8540ad
Khoidm <133933241+kathype2004@users.noreply.github.com>
2026-05-29 16:54:53 +0700

    bỏ import thừa

--- monolith-service/src/main/java/com/project/tshop/entity/RevokedRefreshToken.java
+++ monolith-service/src/main/java/com/project/tshop/entity/RevokedRefreshToken.java
@@ -7,9 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.hibernate.annotations.UuidGenerator;
 
 import java.time.Instant;
